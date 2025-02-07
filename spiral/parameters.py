e6f8fee59b6da3687439938964d8241fec4555cb
EdwardBerman <10emb29@gmail.com>
2025-02-07 13:21:22 -0500

    my directory default

--- spiral/parameters.py
+++ spiral/parameters.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import argparse
+import os
 
 def strToBool(value):
     if value.lower() in {'false', 'f', '0', 'no', 'n'}:
@@ -14,6 +15,9 @@ def noneOrStr(value):
         return None
     return value
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+output_directory = os.path.join(current_directory, 'output')
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='mlp')
 parser.add_argument('--seed', type=int, default=0)
@@ -21,7 +25,7 @@ parser.add_argument('--device', type=str, default='cuda:1')
 parser.add_argument('--cr', type=float, default=0.)
 parser.add_argument('--icr', type=float, default=0.)
 parser.add_argument('--n_data', type=int, default=200)
-parser.add_argument('--log_pre', type=str, default='/tmp')
+parser.add_argument('--log_pre', type=str, default=output_directory)
 
 args = parser.parse_args()
 model = args.model
